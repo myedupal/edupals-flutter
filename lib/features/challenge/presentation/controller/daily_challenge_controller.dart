28e012295cd302903c7e9e7d739efee3959e197b
ang202 <angkaithong2018@gmail.com>
2024-05-25 23:12:28 +0800

    Update mcq icon

--- lib/features/challenge/presentation/controller/daily_challenge_controller.dart
+++ lib/features/challenge/presentation/controller/daily_challenge_controller.dart
@@ -1,5 +1,4 @@
 import 'package:edupals/core/base/base_controller.dart';
-import 'package:edupals/core/base/model/query_params.dart';
 import 'package:edupals/features/challenge/domain/model/challenge.dart';
 import 'package:edupals/features/challenge/domain/repository/challenge_repository.dart';
 import 'package:get/get.dart';
@@ -17,9 +16,9 @@ class DailyChallengeController extends BaseController {
   Future<void> getChallenges() async {
     setLoading();
     await challengeRepo.getChallenges(
-        queryParams: QueryParams(
-            fromStartAt: "2024-03-01T00:00:00+08:00",
-            toStartAt: "2024-03-31T00:00:00+08:00"),
+        // queryParams: QueryParams(
+        //     fromStartAt: "2024-03-01T00:00:00+08:00",
+        //     toStartAt: "2024-03-31T00:00:00+08:00"),
         onSuccess: (value) {
           if (value?.isEmpty == true) {
             setNoData();
