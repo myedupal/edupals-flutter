2b199da3f697c2915d8c1f0281b4c064167baecf
Kai <angkaithong2018@gmail.com>
2024-05-26 18:23:43 +0800

    Change letter spacing

--- lib/core/values/app_text_style.dart
+++ lib/core/values/app_text_style.dart
@@ -14,6 +14,7 @@ class BaseTextStyle extends TextStyle {
           leadingDistribution: TextLeadingDistribution.even,
           decoration: decoration,
           fontFamily: fontFamily,
+          letterSpacing: 0,
         );
 }
 
